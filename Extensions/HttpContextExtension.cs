3e8144c80211404e3a5824e6c1c981ccb4d96483
Gabriel Souza Batista <gabriel.batista@suporterenova.onmicrosoft.com>
2024-04-19 14:44:35 -0300

    implementa limite de requisição por rota em um determinado tempo em segundos

---
+++ Extensions/HttpContextExtension.cs
@@ -0,0 +1,16 @@
+﻿using MinimalApi.RateLimit;
+
+namespace MinimalApi.Extensions
+{
+    public static class HttpContextExtension
+    {
+        public static string GetCustomerKey(this HttpContext context)
+            => $"{context.Request.Path}_{context.Connection.RemoteIpAddress}";
+
+        public static bool HasRateLimitAttribute(this HttpContext context, out RateLimitAttribute? rateLimitAttribute)
+        {
+            rateLimitAttribute = context.GetEndpoint()?.Metadata.GetMetadata<RateLimitAttribute>();
+            return rateLimitAttribute is not null;
+        }
+    }
+}
